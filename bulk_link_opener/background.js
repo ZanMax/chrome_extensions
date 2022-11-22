aaa37fade29a4a407e1954e2b96209cd5b3554a4
maxim <zanoga.maxim@gmail.com>
2022-11-22 10:55:18 -0800

    add gitignore

--- bulk_link_opener/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      console.log(request.url);
-      chrome.tabs.create({"url": request.url});
-    }
-  }
-);
