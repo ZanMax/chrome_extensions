dfac6ed1feaaf9344bde163c594d696e637ad225
maxim <zanoga.maxim@gmail.com>
2022-11-22 10:56:03 -0800

    update urls opener to manifest v3

--- bulk_link_opener/functions.js
+++ bulk_link_opener/functions.js
@@ -1,47 +1,35 @@
-document.addEventListener( "DOMContentLoaded" , function () {
-  document.getElementById("set_data").addEventListener( "click" , act_tab);
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("set_data").addEventListener("click", act_tab);
 });
 
-function act_tab(){
-  var text = $('textarea#data-form').val();
-  var text_split = text.split('\n');
 
-  if (text.length > 0){
-    for(i=0;i<text_split.length;i++){
-      if (text_split[i].length > 1) {
-        if (text_split[i].startsWith('http'))
-        {
-          chrome.runtime.sendMessage({"message": "open_new_tab", "url": text_split[i] });
-        } else {
-          chrome.runtime.sendMessage({"message": "open_new_tab", "url": "http://" + text_split[i] });
+function act_tab() {
+    let text = $('textarea#data-form').val();
+    let text_split = text.split('\n');
+
+    if (text.length > 0) {
+        for (let i = 0; i < text_split.length; i++) {
+            if (text_split[i].length > 1) {
+                if (text_split[i].startsWith('http')) {
+                    chrome.tabs.create({url: text_split[i], active: false});
+                } else {
+                    chrome.tabs.create({url: "http://" + text_split[i], active: false});
+                }
+            }
         }
-      }
     }
-  }
-/*
-  chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-
-  }
-);
-*/
 }
 
-/*
-function act_tab(){
-  var text = $('textarea#data-form').val();
-  var text_split = text.split('\n');
-
-  if (text.length > 0){
-    for(i=0;i<text_split.length;i++){
-      if (text_split[i].length > 1) {
-        if (text_split[i].startsWith('http'))
-        {
-          chrome.tabs.create({ url: text_split[i] });
-        } else {
-          chrome.tabs.create({ url: 'http://' + text_split[i] });
+$("#fileUpload").change(function () {
+    let file = document.getElementById("fileUpload").files[0];
+    if (file) {
+        let reader = new FileReader();
+        reader.readAsText(file, "UTF-8");
+        reader.onload = function (evt) {
+            $('textarea#data-form').val(evt.target.result);
         }
-      }
+        reader.onerror = function (evt) {
+            console.log("error reading file");
+        }
     }
-  }
-} */
+});
