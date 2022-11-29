5244ccf3a225fec9e3ce1195baddfae772e6baef
maxim <zanoga.maxim@gmail.com>
2022-11-28 19:27:17 -0800

    update extensions

--- sites_block_extension/contentscript.js
+++ sites_block_extension/contentscript.js
@@ -1,15 +1,9 @@
-// All rights reserved DevBytes.net
-// You can use this code without permission of the author
-// contact: support@devbytes.net
-
 let url = window.location.href;
 let domain = new URL(url).hostname.replace('www.', '');
 
-let time_start_block = '08'
-let time_stop_block = '23'
-
 let today = new Date();
 let CurrentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
 
 window.onload = function () {
     chrome.storage.local.get(["SiteBlockerSettings"], function (settingsRAW) {
@@ -23,6 +17,9 @@ window.onload = function () {
                 let toTime = settingsData.toTime;
                 let URLtoRedirect = settingsData.URLtoRedirect;
                 let URLStoBlock = settingsData.URLStoBlockArray;
+                if (fromTime.startsWith('0')) {
+                    fromTime = fromTime.replace('0', '');
+                }
                 if (CurrentTime > fromTime && CurrentTime < toTime) {
                     if (URLStoBlock.includes(domain)) {
                         if (URLtoRedirect.startsWith('http')) {
